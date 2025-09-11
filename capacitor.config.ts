fe8a5e750ce41cac5e0b9be35f22dba201fbecc4
Ayur Pulle <ayurpulle.2004@gmail.com>
2025-09-11 11:03:47 +0100

    ios app build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.6434c475678747b5b1385513ddf95649',
-  appName: 'wayfarer-weave',
+  appId: 'com.ayurpulle.iter',
+  appName: 'iter',
   webDir: 'dist',
   server: {
     url: 'https://6434c475-6787-47b5-b138-5513ddf95649.lovableproject.com?forceHideBadge=true',
